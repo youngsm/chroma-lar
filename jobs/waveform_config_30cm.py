64de3484eade2305cb93dfff9d9f989831efbbeb
Sam Young <youngsam@stanford.edu>
2025-11-21 15:18:30 -0800

    update wvfm config 3 cm / 30 cm with updated sim times

--- jobs/waveform_config_30cm.py
+++ jobs/waveform_config_30cm.py
@@ -1,3 +1,11 @@
+import os
+
+"""
+10M: mean = 6.356 s, std = 0.811 s
+15M: mean = 9.168 s, std = 0.779 s
+20M: mean = 11.957 s, std = 1.190 s
+25M: mean = 14.880 s, std = 0.727 s
+"""
 # Detector and voxel configuration
 config = {
     # Detector dimensions (mm)
@@ -7,15 +15,38 @@ config = {
     # Voxel parameters
     "voxel_size": 300,  # mm
     # Simulation parameters
-    "nphotons": 200_000,
+    "nphotons": 15_000_000,
     "detector_config": "detector_config_reflect_reflect3wires",
     # Job parameters
-    "time_per_voxel": 15,  # seconds per voxel
+    "time_per_voxel": 9.168,  # seconds per voxel
     "max_job_time": 3 * 60 * 60,  # 3 hours in seconds
     "slurm_max_job_time_buffer": 2 * 60,  # 2 minutes in seconds
-    "output_dir": "waveform_maps",
-    # Singularity container
-    "container": "/sdf/home/y/youngsam/sw/dune/sim/chroma-lar/installation/chroma3.lar-plib/chroma.simg",
-    "partition": "ampere",
-    "account": "neutrino:cider-nu",
+    "site": "slac",
 }
+
+
+site = dict(
+    slac=dict(
+        work_dir="$LSCRATCH",
+        container_cmd="singularity exec --nv -B /lscratch,/sdf /sdf/home/y/youngsam/sw/dune/sim/chroma-lar/installation/chroma3.lar-plib/chroma.simg",
+        output_dir=f"/sdf/data/neutrino/{os.environ['USER']}/prod_chroma_lar/waveform_map",
+        slurm=dict(
+            partition='ampere',
+            account='neutrino:cider-nu',
+            output=f"/sdf/data/neutrino/{os.environ['USER']}/prod_chroma_lar/logs/wfmap_%A_%a.log",
+            error=f"/sdf/data/neutrino/{os.environ['USER']}/prod_chroma_lar/logs/wfmap_%A_%a.log",
+        ),
+    ),
+    perlmutter=dict(
+        work_dir="$PSCRATCH",
+        container_cmd="shifter --image=deeplearnphysics/simlar:larchroma-2025-07-18",
+        output_dir=f"/global/cfs/cdirs/dune/users/{os.environ['USER']}/prod_chroma_lar/waveform_map",
+        slurm=dict(
+            account='dune',
+            output=f"/global/cfs/cdirs/dune/users/{os.environ['USER']}/prod_chroma_lar/logs/wfmap_%A_%a.log",
+            error=f"/global/cfs/cdirs/dune/users/{os.environ['USER']}/prod_chroma_lar/logs/wfmap_%A_%a.log",
+            qos='shared',
+            constraint='gpu',
+        )
+    )
+)
